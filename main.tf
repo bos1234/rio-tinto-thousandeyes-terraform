a2fc6423b441fd90a14448ca18d32ca17f9f3419
bos1234 <suren130@gmail.com>
2025-06-23 20:20:35 +1000

    Change interval to 180 to trigger terraform apply and reflect in ThousandEyes

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "thousandeyes_http_server" "rio_http_test" {
   test_name          = "RioTinto HTTP Server Test"
   agents             = [55962]  # Replace with your actual agent IDs
   url                = "https://www.example.com"
-  interval           = 300
+  interval           = 180 
   verify_certificate = true
 }
