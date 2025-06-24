77ea171aa60786011c18260fe39dd9b380ce2971
bos1234 <suren130@gmail.com>
2025-06-24 12:54:04 +1000

    changed agent id

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "thousandeyes_http_server" "rio_http_test" {
   test_name          = "RioTinto HTTP Server Test"
-  agents             = [55962] # Replace with your actual agent IDs
+  agents             = [9024] # Replace with your actual agent IDs
   url                = "https://www.example.com"
   interval           = 300 
   verify_certificate = true
